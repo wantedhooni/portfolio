ee85c84f01863909f114c1f533e1959b5e47e8ce
Revy <wantedhooni@gmail.com>
2026-05-20 16:22:26 +0900

    no message

--- core-banking/backend/script/app-all-stop.sh
+++ core-banking/backend/script/app-all-stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-kill -9 $(ss -lntp '( sport = :8091 )' | grep -oP 'pid=\K\d+') || true
-kill -9 $(ss -lntp '( sport = :8091 )' | grep -oP 'pid=\K\d+') || true
+kill -9 $(ss -lntp '( sport = :18081 )' | grep -oP 'pid=\K\d+') || true
+kill -9 $(ss -lntp '( sport = :18091 )' | grep -oP 'pid=\K\d+') || true
