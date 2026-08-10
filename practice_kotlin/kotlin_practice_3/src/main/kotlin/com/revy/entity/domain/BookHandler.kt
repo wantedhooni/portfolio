f7ed6ec4e61a11cc77128d010a5d14a05cf7fb2e
Revy <wantedhooni@gmail.com>
2026-08-10 20:11:58 +0900

    feat: 책 관리 기능 프로젝트 초기 설정 추가
    - 빌드 환경 및 Gradle 설정 파일 생성
    - Docker 컨테이너 오케스트레이션 구성
    - Spring Boot 애플리케이션 메인 클래스 추가
    - Book 엔티티, 리포지토리, DTO 등 도메인 로직 구현
    - QueryDSL 쿼리 최적화 설정
    - 데이터베이스 초기화 실행기를 추가

---
+++ practice_kotlin/kotlin_practice_3/src/main/kotlin/com/revy/entity/domain/BookHandler.kt
@@ -0,0 +1,16 @@
+package com.revy.entity.domain
+
+import com.revy.entity.common.BaseEntityComponent
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+
+@Component
+@Transactional(readOnly = false)
+class BookHandler : BaseEntityComponent() {
+
+    fun createBook(title: String, author: String) : Book {
+        val newBook = Book.createNewBook(title, author)
+        em.persist(Book.createNewBook(title, author))
+        return newBook;
+    }
+}
