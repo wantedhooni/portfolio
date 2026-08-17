a6695c648adb92add09a5ec66b9f863096b955ba
Revy <wantedhooni@gmail.com>
2026-08-17 19:52:43 +0900

    feat: 스프링 배치 Chapter 02 추가
    - HelloWorldBatch.kt: 기본 배치 로직 구현
    - Chapter02Application.kt: 애플리케이션 진입점 추가
    - BatchConfiguration.kt: 스프링 배치 구성 파일 설정
    - application.yml: 애플리케이션 환경 설정
    - build.gradle.kts: Gradle 빌드 구성 추가
    - docker-compose.yml: 컨테이너 오케스트레이션 설정

---
+++ spring_batch/Chapter02/src/main/kotlin/com/revy/batch/chapter_02/Chapter02Application.kt
@@ -0,0 +1,12 @@
+package com.revy.batch.chapter_02
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+
+@SpringBootApplication
+class Chapter02Application
+
+fun main(args: Array<String>) {
+    runApplication<Chapter02Application>(*args)
+}
